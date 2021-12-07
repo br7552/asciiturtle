0637650dfb0c0687e8efc19462e0cfe0de56fb10
Blake <reyno254@unlv.nevada.edu>
2021-12-07 15:17:48 -0800

    Create Pen

--- asciiturtle.go
+++ asciiturtle.go
@@ -1,27 +1,106 @@
 package asciiturtle
 
-import "strings"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
 
-type Grid [][]byte
+type Pen struct {
+	Canvas  Canvas
+	X, Y    int
+	Char    byte
+	Heading int
+}
 
-func NewGrid(x, y int) Grid {
+func NewPen(canvas Canvas, char byte, x, y int) (*Pen, error) {
+	if canvas == nil {
+		return nil, fmt.Errorf("canvas must not be nil")
+	}
+
+	pen := Pen{
+		Canvas: canvas,
+		Char:   char,
+	}
+
+	err := pen.Goto(x, y)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pen, nil
+}
+
+func (p *Pen) Dot() {
+	p.Canvas[p.Y][p.X] = p.Char
+}
+
+func (p *Pen) Goto(x, y int) error {
+	if x < 0 || x >= p.Canvas.Width() || y < 0 || y >= p.Canvas.Height() {
+		return fmt.Errorf("(%d,%d) must be within %dx%d canvas", x, y,
+			p.Canvas.Width(), p.Canvas.Height())
+	}
+
+	p.X = x
+	p.Y = y
+
+	return nil
+}
+
+func (p *Pen) Forward(distance int) {
+	for i := 0; i < distance; i++ {
+		x := p.X + int(math.Round(math.Cos(float64(p.Heading))))
+		y := p.Y + int(math.Round(math.Sin(float64(p.Heading))))
+
+		if err := p.Goto(x, y); err != nil {
+			break
+		}
+
+		p.Dot()
+	}
+}
+
+func (p *Pen) Backward(distance int) {
+	for i := 0; i < distance; i++ {
+		x := p.X - int(math.Round(math.Cos(float64(p.Heading))))
+		y := p.Y - int(math.Round(math.Sin(float64(p.Heading))))
+
+		if err := p.Goto(x, y); err != nil {
+			break
+		}
+
+		p.Dot()
+	}
+}
+
+func (p *Pen) Right(deg int) {
+	p.Heading += deg
+}
+
+func (p *Pen) Left(deg int) {
+	p.Heading -= deg
+}
+
+type Canvas [][]byte
+
+func NewCanvas(x, y int) Canvas {
 	if x <= 0 || y <= 0 {
 		return [][]byte{}
 	}
 
-	grid := make([][]byte, y)
+	canvas := make([][]byte, y)
 
-	for i := range grid {
-		grid[i] = make([]byte, x)
+	for i := range canvas {
+		canvas[i] = make([]byte, x)
 	}
 
-	return grid
+	return canvas
 }
 
-func (g Grid) String() string {
+func (c Canvas) String() string {
 	var b strings.Builder
 
-	for _, row := range g {
+	for _, row := range c {
 		for _, v := range row {
 			if v == 0 {
 				b.WriteByte(' ')
@@ -35,4 +114,16 @@ func (g Grid) String() string {
 	}
 
 	return b.String()
+}
+
+func (c Canvas) Height() int {
+	return len(c)
+}
+
+func (c Canvas) Width() int {
+	if len(c) == 0 {
+		return 0
+	}
+
+	return len(c[0])
 }
--- asciiturtle_test.go
+++ asciiturtle_test.go
@@ -5,39 +5,105 @@ import (
 	"testing"
 )
 
-func TestGrid(t *testing.T) {
+func TestCanvas(t *testing.T) {
 	tests := []struct {
 		name string
 		got  string
 		want string
 	}{
 		{
-			name: "single row grid",
-			got:  fmt.Sprint(NewGrid(4, 1)),
+			name: "single row canvas",
+			got:  fmt.Sprint(NewCanvas(4, 1)),
 			want: "    \n",
 		},
 		{
-			name: "multiple row grid",
-			got:  fmt.Sprint(NewGrid(3, 3)),
+			name: "multiple row canvas",
+			got:  fmt.Sprint(NewCanvas(3, 3)),
 			want: "   \n   \n   \n",
 		},
 		{
-			name: "empty grid",
-			got:  fmt.Sprint(NewGrid(0, 0)),
+			name: "empty canvas",
+			got:  fmt.Sprint(NewCanvas(0, 0)),
 			want: "",
 		},
 		{
 			name: "negative dimensions",
-			got:  fmt.Sprint(NewGrid(-1, -3)),
+			got:  fmt.Sprint(NewCanvas(-1, -3)),
 			want: "",
 		},
 	}
 
 	for _, v := range tests {
 		t.Run(v.name, func(t *testing.T) {
-			if v.got != v.want {
-				t.Errorf("Wanted %q, got %q", v.want, v.got)
-			}
+			assertCanvas(t, v.got, v.want)
 		})
+	}
+}
+
+func TestPen(t *testing.T) {
+	canvas := NewCanvas(5, 5)
+
+	pen, err := NewPen(canvas, '#', 0, 0)
+	if err != nil {
+		t.Fatal("got an unexpected error from NewPen")
+	}
+
+	if pen.Heading != 0 {
+		t.Fatal("pen initialized with non-zero heading")
+	}
+
+	pen.Dot()
+
+	got := canvas.String()
+	want := "#    \n" +
+		"     \n" +
+		"     \n" +
+		"     \n" +
+		"     \n"
+
+	assertCanvas(t, got, want)
+
+	pen.Forward(3)
+
+	got = canvas.String()
+	want = "#### \n" +
+		"     \n" +
+		"     \n" +
+		"     \n" +
+		"     \n"
+
+	assertCanvas(t, got, want)
+
+	pen.Backward(2)
+	pen.Right(90)
+	pen.Forward(2)
+
+	got = canvas.String()
+	want = "#### \n" +
+		" #   \n" +
+		" #   \n" +
+		"     \n" +
+		"     \n"
+
+	assertCanvas(t, got, want)
+
+	pen.Left(135)
+	pen.Forward(3)
+
+	got = canvas.String()
+	want = "#### \n" +
+		" ##  \n" +
+		" #   \n" +
+		"     \n" +
+		"     \n"
+
+	assertCanvas(t, got, want)
+}
+
+func assertCanvas(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("\nWanted:\n%sgot:\n%s", want, got)
 	}
 }
